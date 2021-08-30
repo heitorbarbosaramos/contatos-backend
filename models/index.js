4f4145dfe0a9c37c42ef9b1616daeb0a62c2f0ca
heitor.ramos <heitorhfbr@gmail.com>
2021-08-30 13:47:03 -0300

    Criando arquivo de gerencia das entidades no banco

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const dbConfig = require('../config/db.config');
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DATABASE, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.DIALECT,
+    operatorsAliasdes: 0,
+     pool: {
+         max: dbConfig.pool.max,
+         min: dbConfig.pool.min,
+         acquire: dbConfig.pool.acquire,
+         idle: dbConfig.pool.idle
+     }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.endereco = require('./endereco.model')(sequelize, Sequelize);
+
+module.exports = db;
