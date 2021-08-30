2f0d9917998931c35603e1b97bef2be07ed03d34
heitor.ramos <heitorhfbr@gmail.com>
2021-08-30 13:55:44 -0300

    Criando rotas para entidade endereco

---
+++ routers/endereco.routers.js
@@ -0,0 +1,29 @@
+module.exports = app => {
+    const endereco = require("../controllers/endereco.controller");
+
+    var router = require("express").Router();
+
+    // Criando novo endereco
+    router.post("/", endereco.create);
+
+    // Recuperando todos os enderecos
+    router.get("/", endereco.findAll);
+
+    // Recuperando endereco pelo id
+    router.get("/:id", endereco.findOne);
+
+    // Atualizando endereco pelo id
+    router.put("/:id", endereco.update);
+
+    // Deletando endereco pelo id
+    router.delete("/:id", endereco.delete);
+
+    router.get("/teste",  (req, res) => {
+        res.json({ message: "TESTE, MENSAGEM, OK" });
+      });
+
+
+    app.use('/api/endereco', router);
+
+    
+}
